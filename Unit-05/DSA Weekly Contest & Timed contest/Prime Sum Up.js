b87d555976bce111b3a1412b898a275631c112d5
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2023-01-25 23:15:36 +0530

    Create Prime Sum Up.js

---
+++ Unit-05/DSA Weekly Contest & Timed contest/Prime Sum Up.js
@@ -0,0 +1,49 @@
+// Description
+
+// You are given a number, stored in a variable with the namenum. Find out the sum of all prime numbers in the range of [1,num], including the value stored innum
+
+
+// Input
+// The first and the only line of the input contains the value stored innum
+
+
+// Output
+// Print the sum of all prime numbers in the range of[1,num], includingnumitself
+
+
+// Sample Input 1 
+
+// 13
+// Sample Output 1
+
+// 41
+// Hint
+
+// In the sample test case, the value stored innum = 13. Therefore, all the prime numbers, in the range of[1,num]are
+
+// 2
+// 3
+// 5
+// 7
+// 11
+// 13
+// Therefore, the sum of prime numbers becomes,2 + 3 + 5 + 7 + 11 + 13 = 41, which is the required answer
+
+function primeSumUp(num){
+    var sum = 0;
+    for(var i = 2; i<=num; i++){
+        var out = "true";
+        for(var j = 2; j<i; j++){
+            if(i % j == 0){
+               out = "false";
+               break;
+            }
+
+        }
+        if(out == "true"){
+            sum += i;
+           }
+        }
+        console.log(sum);
+    }
+   
